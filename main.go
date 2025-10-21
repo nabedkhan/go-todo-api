fef331deef1ff1bd76e6fb9a5c7bfac31579df1b
Nabed Khan <nabed420@gmail.com>
2025-10-21 17:59:22 +0600

    Added headers for avoiding CORS in the HeadersMiddleware

--- main.go
+++ main.go
@@ -137,7 +137,14 @@ func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
 
 func HeadersMiddleware(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		// Standard JSON Response Header
 		w.Header().Set("Content-Type", "application/json")
+
+		// CORS Headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 		next(w, r)
 	}
 
